28b2c2dd000347ee50778a0f9beddddb26f75139
nofendian17 <bennie.anware@gmail.com>
2024-04-24 02:04:48 +0700

    refactor : signal

--- cmd/server.go
+++ cmd/server.go
@@ -3,11 +3,12 @@ package cmd
 import (
 	"context"
 	"fmt"
-	"github.com/nofendian17/gostarterkit/internal/container"
-	"github.com/nofendian17/gostarterkit/internal/delivery/rest"
 	"os"
 	"os/signal"
 	"syscall"
+
+	"github.com/nofendian17/gostarterkit/internal/container"
+	"github.com/nofendian17/gostarterkit/internal/delivery/rest"
 )
 
 type Server interface {
@@ -21,26 +22,21 @@ type server struct {
 
 // StartRestServer starts the rest server.
 func (s *server) StartRestServer(ctx context.Context) error {
-	// Channel to catch errors
-	errCh := make(chan error)
 
 	// Start REST server in a separate goroutine
 	go func() {
-		errCh <- s.rest.Start(s.cntr.Config.Application.Port)
+		err := s.rest.Start(s.cntr.Config.Application.Port)
+		if err != nil {
+			s.cntr.Logger.Error(ctx, "Failed to start server", err)
+		}
 	}()
 
 	// Handle OS signals for graceful shutdown
 	signalCh := make(chan os.Signal, 1)
 	signal.Notify(signalCh, os.Interrupt, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
 
-	select {
-	case sig := <-signalCh:
-		errCh <- fmt.Errorf("received signal %v", sig)
-	case err := <-errCh:
-		if err != nil {
-			s.cntr.Logger.Error(ctx, "Got error signal", err)
-		}
-	}
+	sig := <-signalCh
+	s.cntr.Logger.Info(ctx, fmt.Sprintf("Received signal: %s", sig), nil)
 
 	// Stop the REST server
 	if err := s.rest.Stop(context.Background()); err != nil {
